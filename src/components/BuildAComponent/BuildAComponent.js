0e66905f4c7ffb2b0402de25faf828ba8cca116b
swaseem117 <62356112+swaseem117@users.noreply.github.com>
2020-06-19 16:55:55 -0400

    init

--- src/components/BuildAComponent/BuildAComponent.js
+++ src/components/BuildAComponent/BuildAComponent.js
@@ -9,39 +9,56 @@ export default function BuildAComponent() {
 
     const changeHanlder = (e) => {
        
-       let cleanValue = e.target.value.trim().replace(/\s\s+/g,"").substr(0, 19);
-       const reg = RegExp(/[0-9\s]+$/,"g");
-       let isValid = reg.test(cleanValue) && cleanValue.length < 20;
-             
+       let cleanValue = e.target.value.trim().replace(/\s\s+/g," ");
+       cleanValue = cleanValue && (cleanValue[0] == 4 || cleanValue[0] ==  5) ? cleanValue.substr(0, 19) : cleanValue.substr(0, 17);
+       const reg = RegExp(/^[0-9\s]+$/,"g");
+       let isValid = reg.test(cleanValue);
+             console.log(isValid,cleanValue);
        if (isValid) {
-       	  
-       	 let arr = cleanValue.split("");
-       	 console.log('changeHanlder', e.target.value,isValid);
-       	 if (cleanValue.charAt(0) == 4 || 
-       	 	cleanValue.charAt(0) == 3) {
+          
+         let arr = cleanValue.split("");
+         
+         if (cleanValue.charAt(0) == 4 || 
+          cleanValue.charAt(0) == 5) {
 
-       	   if (arr.length === 17) {
-	       	    cleanValue = arr.join("");
-       	   }
-       	   else if (arr.length === 14) {
-	       	    arr.splice(14, 0," ");
-	       	    cleanValue = arr.join("");
-       	   }
-       	   else if (arr.length === 9) {
-	       	    arr.splice(9, 0," ");
-	       	    cleanValue = arr.join("");
-       	   }
-       	   else if (arr.length === 4) {
-	       	    arr.splice(4, 0," ");
-	       	    cleanValue = arr.join("");
-       	   }
-       	   setCCNumber(cleanValue);
-	      
-	    }
-	}
+           if (arr.length === 17) {
+              cleanValue = arr.join("");
+           }
+           else if (arr.length === 14) {
+              arr.splice(14, 0," ");
+              cleanValue = arr.join("");
+           }
+           else if (arr.length === 9) {
+              arr.splice(9, 0," ");
+              cleanValue = arr.join("");
+           }
+           else if (arr.length === 4) {
+              arr.splice(4, 0," ");
+              cleanValue = arr.join("");
+           }
+         }
+
+         else if (cleanValue.charAt(0) == 3) {
+
+           if (arr.length === 17) {
+              cleanValue = arr.join("");
+           }
+           else if (arr.length === 12) {
+              arr.splice(12, 0," ");
+              cleanValue = arr.join("");
+           }
+           else if (arr.length === 5) {
+              arr.splice(5, 0," ");
+              cleanValue = arr.join("");
+           }
+         }
+           setCCNumber(cleanValue);
+        
+      }
+  
        else {
-	        setCCNumber(cleanValue);
-	       	return false;
+
+          return false;
        }
 
     }
@@ -49,7 +66,7 @@ export default function BuildAComponent() {
     return (
          <div className={styles.BuildAComponent}>
          <div className={styles.InputWrapper}>
-         <label htmlFor="ccField">Credit Card Visa/Master</label>
+         <label htmlFor="ccField">Credit Card Visa/Master/American Express</label>
          <input type="text" onChange={changeHanlder} value={ccNumber} id="ccField"/>
          </div>
          </div>
